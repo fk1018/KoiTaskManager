6b38a3dfa9fbc3a5d9f3c2fc9bbe10248cc5bc5d
fk <fredk1018@gmail.com>
2019-10-20 17:48:58 -0500

    tests

--- models/Proxy.ts
+++ models/Proxy.ts
@@ -2,10 +2,10 @@ import {ProxyList} from './ProxyList';
 import {Connection} from './Connection';
 
 export interface Proxy {
+  connections?: Connection[];
   id: string;
+  lists?: ProxyList[];
   password?: string;
   server: string;
   userName?: string;
-  lists?: ProxyList[];
-  connections: Connection[];
 }
