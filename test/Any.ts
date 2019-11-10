076fb2e0de505be541812e91b4e3bce42a901f02
fk <fredk1018@gmail.com>
2019-11-10 11:28:19 -0600

    linting

--- test/Any.ts
+++ test/Any.ts
@@ -1,9 +1,5 @@
 import * as faker from 'faker';
 import { Task } from '../models/Task';
-// import { Site } from '../models/Site';
-// import { Proxy } from '../models/Proxy';
-// import { ProxyList } from '../models/ProxyList';
-//import { Connection } from '../models/Connection';
 
 export const Any = {
   arrayOf<T>(generator: () => T, count: number): T[] {
@@ -23,7 +19,7 @@ export const Any = {
       id: faker.random.uuid(),
       name: faker.random.word(),
       site: faker.random.uuid(),
-      proxyList: faker.random.uuid()
+      proxyList: faker.random.uuid(),
     }
   ): Task {
     return {
@@ -34,27 +30,6 @@ export const Any = {
     };
   },
 };
-
-// function task(
-//   {
-//     id = faker.random.uuid(),
-//     name = faker.random.word(),
-//     site = faker.random.uuid(),
-//     proxyList = faker.random.uuid(),
-//   }: TaskParams = {
-//     id: faker.random.uuid(),
-//     name: faker.random.word(),
-//     site: faker.random.uuid(),
-//     proxyList: faker.random.uuid()
-//   }
-// ): Task {
-//   return {
-//     id,
-//     name,
-//     site,
-//     proxyList,
-//   };
-// }
 
 type TaskParams = {
   id?: string;
--- test/Task.test.ts
+++ test/Task.test.ts
@@ -1,22 +1,22 @@
-import {Any} from './Any';
+import { Any } from './Any';
 
-describe("task interface",()=>{
-  it("has an id",()=>{
+describe('task interface', () => {
+  it('has an id', () => {
     const task = Any.Task();
 
     expect(task.id).toBeTruthy();
   });
-  it("has a name",()=>{
+  it('has a name', () => {
     const task = Any.Task();
 
     expect(task.name).toBeTruthy();
   });
-  it("has a site",()=>{
+  it('has a site', () => {
     const task = Any.Task();
 
     expect(task.site).toBeTruthy();
   });
-  it("has a proxy list",()=>{
+  it('has a proxy list', () => {
     const task = Any.Task();
 
     expect(task.proxyList).toBeTruthy();
