1fa506b0f425f94cc9d64aeb77063453957fad9d
fk <fredk1018@gmail.com>
2019-11-10 20:01:16 -0600

    remove unused

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,9 @@
 import * as puppeteer from 'puppeteer';
 import { TaskManager } from '../models/TaskManager';
-import { Page } from '../models/Page';
 
 (async puppeteer => {
   const browser = await puppeteer.launch();
   const taskManager: TaskManager = { browser, id: '1' };
-  
+
   await browser.close();
 })(puppeteer);
