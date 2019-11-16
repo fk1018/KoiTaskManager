c6ad39f556c15b370ff6008d59d0a0d8bd9a76b1
fk <fredk1018@gmail.com>
2019-11-16 10:43:27 -0600

    ref by id

--- models/Task.ts
+++ models/Task.ts
@@ -1,9 +1,8 @@
-import { Page } from './Page';
 export interface Task {
   id: string;
   name: string;
   site: string;
   proxy?:string;
   proxyList: string[];
-  page?: Page;
+  page?: string;
 }
