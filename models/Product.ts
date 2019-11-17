334a6da79d6cc6fe6fdc1b4b3b4987803b9f2d58
fk <fredk1018@gmail.com>
2019-11-17 00:57:16 -0600

    product model

---
+++ models/Product.ts
@@ -0,0 +1,11 @@
+export interface Product {
+  id: string;
+  productId?: string[];
+  variants?: string[];
+  sizes?: string[];
+  positiveKeywords?: string[];
+  negativeKeywords?: string[];
+  url?:string;
+  imgUrl?:string;
+  styleCode?:string;
+}
