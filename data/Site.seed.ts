b49786387c145e2c219949b6e1add664e0016d90
fk <fredk1018@gmail.com>
2019-11-24 10:20:30 -0600

    site seed

---
+++ data/Site.seed.ts
@@ -0,0 +1,7 @@
+import {Site} from "../models/Site";
+export const Sites : Site[] = [
+  {
+    id:"3a42f014-5ddf-46e0-8a8b-d42474ec1f97",
+    name:"yeezy supply"
+  }
+]
---
+++ data/seed.ts
@@ -0,0 +1,4 @@
+const Store = require('electron-store');
+import {Sites} from "./Site.seed";
+
+const store = new Store();
