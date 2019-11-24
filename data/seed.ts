cf1f2a71b2405ad2e63ceeb61c4db8985327bf2b
fk <fredk1018@gmail.com>
2019-11-24 10:57:13 -0600

    seed data

--- data/seed.ts
+++ data/seed.ts
@@ -4,7 +4,13 @@ import {Proxy} from "../models/Proxy";
 import {ProxyList} from "../models/ProxyList";
 import {Site} from "../models/Site";
 import {Task} from "../models/Task";
+import {TaskStatus} from "../models/TaskStatus";
 
+const Products : Product[] = [];
+const Proxys : Proxy[] = [];
+const ProxyLists: ProxyList[] = [];
+const Sites: Site[] = [];
 const Tasks : Task[] = [];
+const TaskStatus : TaskStatus[] = [];
 
 const store = new Store();
