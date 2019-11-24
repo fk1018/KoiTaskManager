b80166b45fb6a43a381feceb37d2f7e2633138fc
fk <fredk1018@gmail.com>
2019-11-24 10:26:01 -0600

    added seed for product

---
+++ data/Product.seed.ts
@@ -0,0 +1,9 @@
+import {Product} from "../models/Product";
+export const Products : Product[] = [
+  {
+    id:"3a42f014-5ddf-46e0-8a8b-d42474ec1f97",
+    name:"yeezy supply",
+    productId:"FW4839",
+    sizes:["10","12"]
+  }
+]
--- data/seed.ts
+++ data/seed.ts
@@ -1,4 +1,4 @@
 const Store = require('electron-store');
 import {Sites} from "./Site.seed";
-
+import {Products} from "./Product.seed";
 const store = new Store();
