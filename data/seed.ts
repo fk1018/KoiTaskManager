fd854c2aa89bd34d7af85f5fb1fe6d8fb701942e
fk <fredk1018@gmail.com>
2019-11-24 11:23:28 -0600

    TaskStatus seeding

--- data/seed.ts
+++ data/seed.ts
@@ -1,4 +1,5 @@
 import Store from 'electron-store';
+import * as faker from 'faker';
 import { Product } from '../models/Product';
 import { Proxy } from '../models/Proxy';
 import { ProxyList } from '../models/ProxyList';
@@ -14,11 +15,44 @@ const Tasks: Task[] = [];
 const TaskStatus: TaskStatus[] = [];
 const store = new Store();
 
-//Create Task Status
-//Create Product
-//Create Site
-//Create ProxyList
-//Create Proxy
-//Create Task
+//Create Task Status's
+const idle : TaskStatus = {
+  id: faker.random.uuid(),
+  display:"Idle"
+}
+const starting : TaskStatus = {
+  id: faker.random.uuid(),
+  display:"Starting Task"
+}
+const inLine: TaskStatus = {
+  id: faker.random.uuid(),
+  display:"In line"
+}
+const notFound : TaskStatus = {
+  id: faker.random.uuid(),
+  display:"Item not found retrying"
+}
+const addingToCart : TaskStatus = {
+  id: faker.random.uuid(),
+  display:"Adding item to cart"
+}
+const checkingOut : TaskStatus = {
+  id: faker.random.uuid(),
+  display:"CheckingOut"
+}
+const success : TaskStatus = {
+  id: faker.random.uuid(),
+  display:"Success"
+}
+const outOfStock : TaskStatus = {
+  id: faker.random.uuid(),
+  display:"Out of stock retrying"
+}
+
+//Create Product's
+//Create Site's
+//Create ProxyList's
+//Create Proxy's
+//Create Task's
 
 //Store data yusing eletron store
